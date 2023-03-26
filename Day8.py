0ca58c417273a4f4d01f9bcfcab7448efb83b26a
Nick Agiazis <68507786+DocHolidayHunter@users.noreply.github.com>
2023-03-26 21:23:05 +1100

    added day 8 progress

--- Day8.py
+++ Day8.py
@@ -1,4 +1,20 @@
 # The project we are working on today will be to make a caesar cipher.
 # Functions with inputs and Arguments and Paramaters.\
+
+
+
     
+"""
+def greet():
+    print("Welcome")
+    print("Booga")
+    print("Ooga")   
+greet()
+"""
+# Function that allows for us to use an input to pass it over.
+
+def greet_name(name):
+    print(f"How do you do {name}")
     
+greet_name("Nick")
+
