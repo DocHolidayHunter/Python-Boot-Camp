a781ecf124d53419c075c723c8228d4def272d59
Nick Agiazis <68507786+DocHolidayHunter@users.noreply.github.com>
2023-03-28 10:43:19 +1100

    fixed page

--- Day8.py
+++ Day8.py
@@ -12,10 +12,33 @@ def greet():
 greet()
 """
 # Function that allows for us to use an input to pass it over.
-# Can use difference variables to make multi - 
+# Can use difference variables to make multi - variable inputs.
+# This is a positional argument 
+
+# def greet_name(name,loaction):
+#    print(f"How do you do {name}.")
+#    print(f"What is it like in {loaction}.")
+
+# This is a keyword argument which is where we assign a key to the word so that we can use it in any order.
 def greet_name(name,loaction):
     print(f"How do you do {name}.")
     print(f"What is it like in {loaction}.")
     
-greet_name("Nick","Melbourne")
+greet_name(name = "Nick", loaction = "Melbourne")
+
+import math 
+
+def paint_calc(height, width, cover):
+    area = height * width
+    cans = math.ceil(area / coverage)
+
+    print(f"You'll need {cans} cans of paint.")
+
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
+
 
