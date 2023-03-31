4c634d7b11c028745b47367d44f74f8d89d836a1
Nick Agiazis <68507786+DocHolidayHunter@users.noreply.github.com>
2023-03-31 21:36:25 +1100

    started day 9

---
+++ Day9.py
@@ -0,0 +1,26 @@
+# This day focuses on building a silent auction which as many people who want
+# can bid on the program and the highest bid wins and gets displayed.
+
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+
+student_grades = {} # Nested lists needs to have the squiggly brackets
+
+for student in student_scores:
+  score = student_scores[student]
+  if score > 90:
+    student_grades[student] = "Outstanding"
+  elif score > 80:
+    student_grades[student] = "Exceeds Expectations"
+  elif score > 70:
+    student_grades[student] = "Acceptable"
+  else:
+    student_grades[student] = "Fail"
+
+
+print(student_grades)
